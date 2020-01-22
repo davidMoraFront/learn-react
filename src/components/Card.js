0b811c8bae9fcb330d8ac149d1518f0df6ab1e3d
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-01-22 11:55:22 +0100

    add state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ExerciseImg from "../images/exercise.png";
 // import CirlesImg from "../images/circles.png";
+import GafasSolImg from "../images/gafasSol.png";
 import "../styles/Card.scss";
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: GafasSolImg
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        image: ExerciseImg
+      });
+    }, 5000);
+  }
+
   render() {
     const { title, description, img, leftColor, rightColor } = this.props;
     return (
@@ -16,7 +32,7 @@ class Card extends React.Component {
         >
           <div>
             <div className="Fitness-Card-Image">
-              <img src={ExerciseImg} alt="img" />
+              <img src={this.state.image} alt="img" />
             </div>
             <div className="Fitness-Card-Info">
               <h1>{title}</h1>
