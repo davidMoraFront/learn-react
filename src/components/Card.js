c37ba5944f9ebf6ac64da2f4c1b841ed71b6655f
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-01-24 10:31:30 +0100

    add component-list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,10 @@
 import React from "react";
-import ExerciseImg from "../images/exercise.png";
-// import CirlesImg from "../images/circles.png";
-import GafasSolImg from "../images/gafasSol.png";
+// import ExerciseImg from "../images/exercise.png";
+import CirlesImg from "../images/circles.png";
+// import GafasSolImg from "../images/gafasSol.png";
 import "../styles/Card.scss";
 
 class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: GafasSolImg
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        image: ExerciseImg
-      });
-    }, 5000);
-  }
-
   render() {
     const { title, description, img, leftColor, rightColor } = this.props;
     return (
@@ -27,12 +12,12 @@ class Card extends React.Component {
         <div
           className="Fitness-Card"
           style={{
-            background: `url(${img}), no-repeat, linear-gradient(to right, ${leftColor}, ${rightColor}`
+            background: `url(${CirlesImg}), no-repeat, linear-gradient(to right, ${leftColor}, ${rightColor}`
           }}
         >
           <div>
             <div className="Fitness-Card-Image">
-              <img src={this.state.image} alt="img" />
+              <img src={img} alt="img" />
             </div>
             <div className="Fitness-Card-Info">
               <h1>{title}</h1>
