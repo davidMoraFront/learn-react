2334d44c789030f86496f080a24924fa87c59f63
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-01-28 14:55:49 +0100

    add loading, error and redirection

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -2,6 +2,7 @@ import React from "react";
 import ExerciseForm from "../components/ExerciseForm";
 import Card from "../components/Card";
 import "../styles/ExerciseNew.scss";
+import FatalError from "./FatalError";
 
 class ExerciseNew extends React.Component {
   state = {
@@ -11,7 +12,9 @@ class ExerciseNew extends React.Component {
       img: "",
       leftColor: "",
       rightColor: ""
-    }
+    },
+    loading: false,
+    error: null
   };
 
   handleChange = e => {
@@ -24,6 +27,9 @@ class ExerciseNew extends React.Component {
   };
 
   handleSubmit = async e => {
+    this.setState({
+      loading: true
+    });
     e.preventDefault();
     try {
       let config = {
@@ -36,11 +42,21 @@ class ExerciseNew extends React.Component {
       };
       let res = await fetch("http://localhost:8000/api/exercises", config);
       let json = await res.json();
+      this.setState({
+        loading: false
+      });
       console.log(json);
-    } catch (error) {}
+      this.props.history.push("/exercise");
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error
+      });
+    }
   };
 
   render() {
+    if (this.state.error) return <FatalError />;
     return (
       <div className="ExerciseNew-container">
         <Card {...this.state.form} />
