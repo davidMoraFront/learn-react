81fb30dd110adf1ebad0f82746e64a50714978ba
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-01-28 08:58:06 +0100

    init form

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,12 +1,79 @@
 import React from "react";
 
 class ExerciseNew extends React.Component {
-  handleClick = () => {
-    console.log("Clicked");
+  state = {};
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  handleChange = e => {
+    console.log(`${e.target.name}: ${e.target.value}`);
+
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
   render() {
-    return <button onClick={this.handleClick}>Send</button>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="title"
+              name="title"
+              onChange={this.handleChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="description"
+              name="description"
+              onChange={this.handleChange}
+              value={this.state.description}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="img"
+              name="img"
+              onChange={this.handleChange}
+              value={this.state.img}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="leftColor"
+              name="leftColor"
+              onChange={this.handleChange}
+              value={this.state.leftColor}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="rightColor"
+              name="rightColor"
+              onChange={this.handleChange}
+              value={this.state.rightColor}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
