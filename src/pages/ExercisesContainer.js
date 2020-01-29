d3085194866770e1fd2895f50fd564a2599876a3
David Mora <davidmora@MacBook-Pro-de-David.local>
2020-01-30 00:27:49 +0100

    add hook useState

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,41 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Loading from "../components/Loading";
 import FatalError from "./FatalError";
 import Exercises from "./Exercises";
 
-class ExercisesContainer extends React.Component {
-  state = {
-    data: [],
-    loading: true,
-    error: null
-  };
+const ExercisesContainer = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async componentDidMount() {
-    await this.fetchExercises();
-  }
+  if (loading) return <Loading />;
+  if (error) return <FatalError />;
+  return <Exercises data={data} />;
+};
 
-  fetchExercises = async () => {
-    try {
-      let res = await fetch("http://localhost:8000/api/exercises");
-      let data = await res.json();
+// class ExercisesContainer extends React.Component {
+//   state = {
+//     data: [],
+//     loading: true,
+//     error: null
+//   };
 
-      this.setState({
-        data,
-        loading: false
-      });
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error
-      });
-    }
-  };
+//   async componentDidMount() {
+//     await this.fetchExercises();
+//   }
 
-  render() {
-    if (this.state.loading) return <Loading />;
-    if (this.state.error) return <FatalError />;
-    return <Exercises data={this.state.data} />;
-  }
-}
+//   fetchExercises = async () => {
+//     try {
+//       let res = await fetch("http://localhost:8000/api/exercises");
+//       let data = await res.json();
+
+//       this.setState({
+//         data,
+//         loading: false
+//       });
+//     } catch (error) {
+//       this.setState({
+//         loading: false,
+//         error
+//       });
+//     }
+//   };
+
+//   render() {
+//     if (this.state.loading) return <Loading />;
+//     if (this.state.error) return <FatalError />;
+//     return <Exercises data={this.state.data} />;
+//   }
+// }
 
 export default ExercisesContainer;
